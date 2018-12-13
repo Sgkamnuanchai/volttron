cc946d347d345d41bdf7b67d623931e9843ee75e
sgkamnuanchai <sgkamnuanchai@gmail.com>
2018-12-13 14:35:27 +0700

    guidevolttron

--- examples/ListenerAgent/listener/agent.py
+++ examples/ListenerAgent/listener/agent.py
@@ -1,40 +1,4 @@
-# -*- coding: utf-8 -*- {{{
-# vim: set fenc=utf-8 ft=python sw=4 ts=4 sts=4 et:
-#
-# Copyright 2017, Battelle Memorial Institute.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# This material was prepared as an account of work sponsored by an agency of
-# the United States Government. Neither the United States Government nor the
-# United States Department of Energy, nor Battelle, nor any of their
-# employees, nor any jurisdiction or organization that has cooperated in the
-# development of these materials, makes any warranty, express or
-# implied, or assumes any legal liability or responsibility for the accuracy,
-# completeness, or usefulness or any information, apparatus, product,
-# software, or process disclosed, or represents that its use would not infringe
-# privately owned rights. Reference herein to any specific commercial product,
-# process, or service by trade name, trademark, manufacturer, or otherwise
-# does not necessarily constitute or imply its endorsement, recommendation, or
-# favoring by the United States Government or any agency thereof, or
-# Battelle Memorial Institute. The views and opinions of authors expressed
-# herein do not necessarily state or reflect those of the
-# United States Government or any agency thereof.
-#
-# PACIFIC NORTHWEST NATIONAL LABORATORY operated by
-# BATTELLE for the UNITED STATES DEPARTMENT OF ENERGY
-# under Contract DE-AC05-76RL01830
-# }}}
+
 
 from __future__ import absolute_import
 
@@ -48,6 +12,17 @@ from volttron.platform.messaging.health import STATUS_GOOD
 from volttron.platform.vip.agent import Agent, Core, PubSub, compat
 from volttron.platform.agent import utils
 from volttron.platform.messaging import headers as headers_mod
+import importlib
+import random
+import json
+import socket
+import pyrebase
+import urllib3
+import time
+import requests
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+import re
+from uuid import getnode
 
 utils.setup_logging()
 _log = logging.getLogger(__name__)
@@ -56,17 +31,28 @@ DEFAULT_MESSAGE = 'Listener Message'
 DEFAULT_AGENTID = "listener"
 DEFAULT_HEARTBEAT_PERIOD = 5
 
+try:
+    config = {
+        "apiKey": "AIzaSyAY_DKJdz09U3OvDaQ4QjF1dEI2yyvxifI",
+        "authDomain": "guidekub-ff865.firebaseapp.com",
+        "databaseURL": "https://guidekub-ff865.firebaseio.com",
+        "storageBucket": "guidekub-ff865.appspot.com",
+    }
+    firebase = pyrebase.initialize_app(config)
+    db = firebase.database()
+except Exception as er:
+    print er
 
-class ListenerAgent(Agent):
+class Gpub(Agent):
     """Listens to everything and publishes a heartbeat according to the
     heartbeat period specified in the settings module.
     """
 
     def __init__(self, config_path, **kwargs):
-        super(ListenerAgent, self).__init__(**kwargs)
+        super(Gpub, self).__init__(**kwargs)
         self.config = utils.load_config(config_path)
-        self._agent_id = self.config.get('agentid', DEFAULT_AGENTID)
-        self._message = self.config.get('message', DEFAULT_MESSAGE)
+        self._agent_id = self.config.get('agentid')
+        self._message = self.config.get('message')
         self._heartbeat_period = self.config.get('heartbeat_period',
                                                  DEFAULT_HEARTBEAT_PERIOD)
         try:
@@ -93,9 +79,9 @@ class ListenerAgent(Agent):
     @Core.receiver('onstart')
     def onstart(self, sender, **kwargs):
         _log.debug("VERSION IS: {}".format(self.core.version()))
-        if self._heartbeat_period != 0:
-            self.vip.heartbeat.start_with_period(self._heartbeat_period)
-            self.vip.health.set_status(STATUS_GOOD, self._message)
+        self.onstart2()
+        print "start"
+
 
     @PubSub.subscribe('pubsub', '')
     def on_match(self, peer, sender, bus,  topic, headers, message):
@@ -106,11 +92,59 @@ class ListenerAgent(Agent):
             "Peer: %r, Sender: %r:, Bus: %r, Topic: %r, Headers: %r, "
             "Message: \n%s", peer, sender, bus, topic, headers,  pformat(message))
 
+    @Core.periodic(5)
+    def deviceMonitorBehavior2(self):
+
+        try:
+            # to get physical address:
+            original_mac_address = getnode()
+            print("MAC Address: " + str(original_mac_address))  # this output is in raw format
+
+            # convert raw format into hex format
+            hex_mac_address = str(":".join(re.findall('..', '%012x' % original_mac_address)))
+            print("HEX MAC Address: " + hex_mac_address)
+            mac = hex_mac_address.replace(':', '')
+            print mac
+        except:
+            mac = "AAAA"
+
+        try:
+            db.child(mac).child('datetime').child("dt").set(
+                datetime.now().replace(microsecond=0).isoformat())
+            print "---------------update firebase ok"
+        except Exception as er:
+            print er
+
+    def onstart2(self):
+
+        print "on match start"
+        try:
+            # to get physical address:
+            original_mac_address = getnode()
+            print("MAC Address: " + str(original_mac_address))  # this output is in raw format
+
+            # convert raw format into hex format
+            hex_mac_address = str(":".join(re.findall('..', '%012x' % original_mac_address)))
+            print("HEX MAC Address: " + hex_mac_address)
+            mac = hex_mac_address.replace(':', '')
+            print mac
+        except:
+            mac = "AAAA"
+
+
+        try:
+            db.child(mac).child('starttime').child("dt").set(
+                datetime.now().replace(microsecond=0).isoformat())
+            print "---------------update firebase ok"
+        except Exception as er:
+            print er
+
+
 
 def main(argv=sys.argv):
     '''Main method called by the eggsecutable.'''
     try:
-        utils.vip_main(ListenerAgent, version=__version__)
+        utils.vip_main(Gpub, version=__version__)
     except Exception as e:
         _log.exception('unhandled exception')
 
